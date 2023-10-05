a6b8b2caff9e3fca9f483a4d01c99042eab74b81
José Pablo Suárez Gómez <suarezgomezjosepablo03@gmail.com>
2023-10-04 20:43:15 -0600

    hola

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+window.onload = () => {
+  console.log('La página se ha cargado completamente');
+  cargarImagenes();
+};
+
 function cargarImagenes() {
   fetch('/src/mocks/data.json')
     .then(response => response.json())
@@ -6,10 +11,12 @@ function cargarImagenes() {
       const rutaLogo = data.imagenes[3].ruta;
       const rutaImagenPrincipal = data.imagenes[4].ruta;
       const codigo = data.codigo;
+      const eventos = obtenerEventos(data);
       mostrarImagenes(imagenes);
       mostrarLogo(rutaLogo);
       mostrarImagenPrincipal(rutaImagenPrincipal);
       ejecutarCodigo(codigo);
+      mostrarEventos(eventos);
     })
     .catch(error => {
       console.error('Error al cargar las imágenes:', error);
@@ -24,6 +31,26 @@ function mostrarImagenes(imagenes) {
   });
 }
 
+function obtenerEventos(data) {
+  return data.events;
+}
+
+function mostrarEventos(eventos) {
+  const eventsContainer = document.querySelector('.events-cards');
+  eventos.forEach(evento => {
+    const eventDiv = crearEventoDiv(evento);
+    eventsContainer.appendChild(eventDiv);
+  });
+}
+
+function crearEventoDiv(evento) {
+  const eventDiv = document.createElement('div');
+  eventDiv.classList.add('item-events');
+  eventDiv.style.backgroundImage = `url(${evento.path})`;
+  eventDiv.innerHTML = `<h3>${evento.title}</h3>`;
+  return eventDiv;
+}
+
 function mostrarLogo(rutaLogo) {
   const logoIco = document.querySelector('.logo-ico');
   logoIco.src = rutaLogo;
@@ -34,8 +61,7 @@ function mostrarImagenPrincipal(rutaImagenPrincipal) {
   mainIco.src = rutaImagenPrincipal;
 }
 
-
-window.onload = () => {
-  console.log('La página se ha cargado completamente');
-  cargarImagenes();
-};
+function ejecutarCodigo(codigo) {
+  const output = eval(codigo);
+  console.log(output);
+}
